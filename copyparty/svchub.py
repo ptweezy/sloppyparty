3888048755f1a3ccde01c8cfb91ce721f322e228
ed <s@ocv.me>
2026-07-05 19:29:32 +0000

    windows fix; closes #1543

--- copyparty/svchub.py
+++ copyparty/svchub.py
@@ -1222,7 +1222,7 @@ class SvcHub(object):
 
         zs = "th_bwrap"
         for k in zs.split(" "):
-            zsl = [x for x in str(getattr(al, k)).split(" ") if x]
+            zsl = [x for x in str(getattr(al, k, "")).split(" ") if x]
             zbl = [x.encode("ascii", "replace") for x in zsl]
             setattr(al, k + "_s", zsl)
             setattr(al, k + "_b", zbl)
